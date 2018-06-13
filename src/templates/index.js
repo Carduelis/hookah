6fc804fedddd3eda21af302d02ce7ee671fc75b9
Carduelis <pavepy@gmail.com>
2018-06-13 21:36:02 +0300

    normalize.css, typography, consistency

--- src/templates/index.js
+++ src/templates/index.js
@@ -5,7 +5,43 @@ import slides from './slides';
 import preloader from './preloader.ejs';
 import data from '../data';
 import slideWrapper from './slideWrapper.ejs';
+import pricedList from './pricedList.ejs';
 import slideHeading from './slideHeading.ejs';
+
+const customSlideData = slide => {
+    console.log(slide.id);
+    switch (slide.id) {
+        case 'index':
+            return {};
+        case 'about':
+            return {
+                slideHeading: slideHeading(slide)
+            };
+        case 'contacts':
+            return {};
+        case 'gallery':
+            return {};
+        case 'menu':
+            return {};
+        case 'menuHookah':
+            return {
+                pricedListBowl: pricedList({
+                    items: data.bowls,
+                    title: 'Чаши'
+                })
+            };
+        case 'menuTea':
+            return {};
+        case 'menuBar':
+            return {};
+        case 'menuPromo':
+            return {};
+        case 'entertainment':
+            return {};
+        default:
+            return {};
+    }
+};
 
 export default main({
     topbar: topbar(data),
@@ -17,7 +53,7 @@ export default main({
                 ...slide,
                 slide: slides[slide.id]({
                     ...data,
-                    slideHeading: slideHeading(slide)
+                    ...customSlideData(slide)
                 })
             });
         })
